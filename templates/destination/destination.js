d1bdd2cead47237e77f9a9610f785388c45c7ed8
timwlee <timothyl@adobe.com>
2024-08-05 08:07:15 -0700

    Polish overall site

--- templates/destination/destination.js
+++ templates/destination/destination.js
@@ -1,8 +1,64 @@
-import { getMetadata } from '../../scripts/aem.js';
+/* eslint-disable */
+import AdobeAemHeadlessClientJs from 'https://cdn.skypack.dev/pin/@adobe/aem-headless-client-js@v3.2.0-R5xKUKJyh8kNAfej66Zg/mode=imports,min/optimized/@adobe/aem-headless-client-js.js';
+import { getConfigValue } from '../../scripts/configs.js';
+import { createOptimizedPicture, getMetadata } from '../../scripts/aem.js';
 
-export default function decorate(block) {
-  // console.log('destination template: ', block)
+async function initialize(slug) {
+  try {
+    const AEM_HOST = await getConfigValue('aem-host');
+    const AEM_GRAPHQL_ENDPOINT = await getConfigValue('aem-graphql-endpoint');
+    const AEM_HEADLESS_CLIENT = new AdobeAemHeadlessClientJs({ serviceURL: AEM_HOST });
+    
+    let dataObj = {};
 
+    dataObj = await AEM_HEADLESS_CLIENT.runPersistedQuery('ac/' + AEM_GRAPHQL_ENDPOINT + ';airportCode=' + slug);
+    const data = dataObj?.data?.hotelOfferList?.items;
+    return data;
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return false;
+  }
+}
+
+export default async function decorate(block) {
   const slug = getMetadata('slug');
   if (!slug) return;
+
+  let data = [];
+  let updatedColumnItems = [];
+  data = await initialize(slug);
+  
+  // create html
+  data.forEach((item) => {
+    const imagePath = item.bannerImage?._path;
+    const parts = imagePath.split('/');
+    let fileName = parts[parts.length - 1];
+    fileName =  fileName.replaceAll('_', '-');
+    const path = `/images/${fileName}`;
+
+    const optimizedDemoImage = createOptimizedPicture(path, item.airportcode, true, [{ width: '1174' }]);
+
+    updatedColumnItems.push(`
+      <div id=${item.airportcode}>
+        <div class="columns-img-col">
+          ${optimizedDemoImage.outerHTML}
+        </div>
+        <div class="info">
+          <p><strong>${item.title}</strong></p>
+          ${item.teaser.html}
+          <a href="${item.linkUrl}" title="${item.title}" class="button">${item.linkLabel}</a>
+        </div>
+      </div>
+    `);
+  });
+
+
+  const firstSection = block.querySelector('.section.hero-container');
+  const airportBannerHTML = updatedColumnItems.join('');
+  const tempContainer = document.createElement('div');
+  tempContainer.classList.add('section', 'hotel-offer-banner');
+  tempContainer.innerHTML = airportBannerHTML
+
+  firstSection.insertAdjacentElement('afterend', tempContainer);
 }
